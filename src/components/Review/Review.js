9f24a5b1d3b291327443bd0e92ce1bdc2ece8a06
forhadmia1 <forhad8613@gmail.com>
2022-04-04 14:27:17 +0600

    review page is done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Review = ({ review }) => {
     const { name, comment, rating, picture } = review;
     return (
-        <div className='shadow-lg rounded'>
+        <div className='shadow-lg rounded mt-4'>
             <div className="flex justify-center"><img className='w-20 h-20 rounded-full' src={picture} alt="" /></div>
             <div className=" p-4">
                 <h2 className='text-2xl font-bold'>{name}</h2>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useReviewdata from '../../hooks/useReviewdata';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+
+    const [reviews] = useReviewdata();
+
     return (
-        <div>
-            this is review page
+        <div className='grid grid-cols-3 gap-6 mt-12 mx-16'>
+            {
+                reviews.map(review => <Review key={review.id} review={review}></Review>)
+            }
         </div>
     );
 };
