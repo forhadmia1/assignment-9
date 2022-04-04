a62d364a8aad9ce7f45a8336dce3f74291ba1361
forhadmia1 <forhad8613@gmail.com>
2022-04-04 20:25:19 +0600

    redisign the bolg content

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 mx-8 gap-8 mt-6'>
-            <div>
+            <div className='shadow-2xl p-4 rounded-md'>
                 <h1 className='text-2xl font-bold'>1. What is context api?</h1>
                 <p className='text-xl mt-4'>Generally we pass a data from parrent to child component, then the child to another one its call props drilling. At this way we pass the data at every level of component tree. context api provides us a way to pass data from parrent to targeted component with out passing props at every level.</p>
             </div>
-            <div>
+            <div className='shadow-2xl p-4 rounded-md'>
                 <h1 className='text-2xl font-bold'>2. What is Symentic tag?</h1>
                 <p className='text-xl mt-4'>Those tag have a meaning full name and which tell about type of content is called symentic tag. Symentic tag make the code easier. These are the example of symentic tag: article, aside, address, header, footer etc.</p>
             </div>
