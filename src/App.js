58360a8d1ddac8c5b20ac0a29dfc6f44c8416ddc
forhadmia1 <forhad8613@gmail.com>
2022-04-04 17:05:39 +0600

    answer are added in the blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
@@ -18,7 +17,6 @@ function App() {
         <Route path='/reviews' element={<Reviews />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/blogs' element={<Blogs />} />
-        <Route path='/about' element={<About />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
